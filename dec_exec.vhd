0b68d90dc7a020173dbd65d9b2a966920343932f
fatmaebrahim <fatmaebrahim3131@gmail.com>
2023-12-04 03:10:01 +0200

    correct till exec_mem with pipeline

--- dec_exec.vhd
+++ dec_exec.vhd
@@ -2,22 +2,37 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 ENTITY dec_exec IS
       PORT (
-            readData0 : INOUT STD_LOGIC_VECTOR (31 DOWNTO 0);
-            readData1 : INOUT STD_LOGIC_VECTOR (31 DOWNTO 0);
-            destReg0 : INOUT STD_LOGIC_VECTOR (2 DOWNTO 0);
-            destReg1 : INOUT STD_LOGIC_VECTOR (2 DOWNTO 0);
-            opCode : INOUT STD_LOGIC_VECTOR (5 DOWNTO 0);
-            WB0 : INOUT STD_LOGIC;
-            WB1 : INOUT STD_LOGIC
+            readData0_IN : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+            readData1_IN : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+            destReg0_IN : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+            destReg1_IN : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+            opCode_IN : IN STD_LOGIC_VECTOR (5 DOWNTO 0);
+            fetchSignals_IN : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+            regFileSignals_IN : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
+            executeSignals_IN : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+            memorySignals_IN : IN STD_LOGIC_VECTOR(6 DOWNTO 0);
 
+            readData0_OUT : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
+            readData1_OUT : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
+            destReg0_OUT : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
+            destReg1_OUT : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
+            opCode_OUT : OUT STD_LOGIC_VECTOR (5 DOWNTO 0);
+            fetchSignals_OUT : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+            regFileSignals_OUT : OUT STD_LOGIC_VECTOR(4 DOWNTO 0);
+            executeSignals_OUT : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+            memorySignals_OUT : OUT STD_LOGIC_VECTOR(6 DOWNTO 0)
       );
 END dec_exec;
 
 ARCHITECTURE arch_dec_exec OF dec_exec IS
 BEGIN
-      -- outData2 <= readData2;
-      -- opCodeOut <= opCodeIn;
-      -- destRegOut1 <= destRegIn1;
-      -- destRegOut2 <= destRegIn2;
-      -- writeBackOut <= writeBackIn;
+      readData0_OUT <= readData0_IN;
+      readData1_OUT <= readData1_IN;
+      destReg0_OUT <= destReg0_IN;
+      destReg1_OUT <= destReg1_IN;
+      opCode_OUT <= opCode_IN;
+      fetchSignals_OUT  <= fetchSignals_IN ; 
+      regFileSignals_OUT <=regFileSignals_IN; 
+      executeSignals_OUT <=executeSignals_IN ;
+      memorySignals_OUT <=memorySignals_IN ;
 END arch_dec_exec;
