d49d43c87d2f727cf8862c06b7fea64d04ea4569
sara <sara.nousir@gmail.com>
2023-12-06 09:21:51 +0200

    fix bug in LDD instruction

--- dec_exec.vhd
+++ dec_exec.vhd
@@ -2,6 +2,7 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 ENTITY dec_exec IS
       PORT (
+            ImmEaValue_IN : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
             readData0_IN : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
             readData1_IN : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
             destReg0_IN : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
@@ -20,28 +21,24 @@ ENTITY dec_exec IS
             fetchSignals_OUT : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
             regFileSignals_OUT : OUT STD_LOGIC_VECTOR(4 DOWNTO 0);
             executeSignals_OUT : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
-            memorySignals_OUT : OUT STD_LOGIC_VECTOR(6 DOWNTO 0)
+            memorySignals_OUT : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
+            ImmEaValue_OUT : OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
       );
 END dec_exec;
 
 ARCHITECTURE arch_dec_exec OF dec_exec IS
-SIGNAL destReg_temp : STD_LOGIC_VECTOR(2 DOWNTO 0);
+      SIGNAL destReg_temp : STD_LOGIC_VECTOR(2 DOWNTO 0);
 BEGIN
       readData0_OUT <= readData0_IN;
       readData1_OUT <= readData1_IN;
       destReg1_OUT <= destReg1_IN;
       opCode_OUT <= opCode_IN;
-      fetchSignals_OUT  <= fetchSignals_IN ; 
-      regFileSignals_OUT <=regFileSignals_IN; 
-      executeSignals_OUT <=executeSignals_IN ;
-      memorySignals_OUT <=memorySignals_IN ;
-	PROCESS (regFileSignals_IN)
-	BEGIN 
-	if regFileSignals_IN(2)='1' then
-	destReg_temp <= destReg0_IN;
-	else
-	destReg_temp <= destReg_temp;
-	end if;
-	END PROCESS;
+      fetchSignals_OUT <= fetchSignals_IN;
+      regFileSignals_OUT <= regFileSignals_IN;
+      executeSignals_OUT <= executeSignals_IN;
+      memorySignals_OUT <= memorySignals_IN;
+      destReg_temp <= destReg0_IN WHEN (regFileSignals_IN(2) = '1') ELSE
+            destReg_temp;
       destReg0_OUT <= destReg_temp;
+      ImmEaValue_OUT <= ImmEaValue_IN;
 END arch_dec_exec;
