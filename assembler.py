b5931ec14704697d72292fd567a9caecd9da3f09
fatmaebrahim <fatmaebrahim3131@gmail.com>
2023-12-16 00:32:34 +0200

    all the two-op except swap

--- assembler.py
+++ assembler.py
@@ -19,6 +19,21 @@ def one_operand_check(instruction):
         return True
      else: 
         return False
+def two_operand_check(instruction):
+     if len(instruction)==3 and len(instruction[1])==2 and instruction[1][0]=='r' and len(instruction[2])==2  and instruction[2][0]=='r' and 0 <= int(instruction[1][1]) <= 7 and 0 <= int(instruction[2][1]) <= 7 :
+        return True
+     else: 
+        return False
+def bitset_one_operand_check(instruction):
+     if len(instruction)==3 and len(instruction[1])==2 and instruction[1][0]=='r' and 0 <= int(instruction[1][1]) <= 7 and 0<=int(instruction[2])<=31  :
+        return True
+     else: 
+        return False
+def imm_one_operand_check(instruction):
+     if len(instruction)==3 and len(instruction[1])==2 and instruction[1][0]=='r' and 0 <= int(instruction[1][1]) <= 7 and -32767 <= int(instruction[2]) <= 32768 :
+        return True
+     else: 
+        return False
 
 def three_operand_check(instruction):
      if len(instruction)==4 and len(instruction[1])==2 and len(instruction[2])==2 and len(instruction[3])==2 and instruction[1][0]=='r' and instruction[2][0]=='r' and instruction[3][0]=='r' and 0 <= int(instruction[1][1]) <= 7 and 0 <= int(instruction[2][1]) <= 7 and 0 <= int(instruction[3][1]) <= 7:
@@ -85,8 +100,93 @@ for instruction in instructions:
         else:
             instruction_string = ' '.join(instruction)
             print("error in instruction "+ instruction_string+" syntax")
-
-        
+    elif instruction[0]=="sub":
+       if three_operand_check(instruction):
+           binary_instruction="010011"
+           reg1=to_binary(instruction[1][1],3)
+           reg2=to_binary(instruction[2][1],3)
+           reg3=to_binary(instruction[3][1],3)
+           binary_instruction=binary_instruction+reg1+reg2+reg3+"0"
+           binary_codes.append(binary_instruction)
+       else:
+           instruction_string = ' '.join(instruction)
+           print("error in instruction "+ instruction_string+" syntax")
+    elif instruction[0]=="and":
+       if three_operand_check(instruction):
+           binary_instruction="010100"
+           reg1=to_binary(instruction[1][1],3)
+           reg2=to_binary(instruction[2][1],3)
+           reg3=to_binary(instruction[3][1],3)
+           binary_instruction=binary_instruction+reg1+reg2+reg3+"0"
+           binary_codes.append(binary_instruction)
+       else:
+           instruction_string = ' '.join(instruction)
+           print("error in instruction "+ instruction_string+" syntax")
+    elif instruction[0]=="or":
+       if three_operand_check(instruction):
+           binary_instruction="010101"
+           reg1=to_binary(instruction[1][1],3)
+           reg2=to_binary(instruction[2][1],3)
+           reg3=to_binary(instruction[3][1],3)
+           binary_instruction=binary_instruction+reg1+reg2+reg3+"0"
+           binary_codes.append(binary_instruction)
+       else:
+           instruction_string = ' '.join(instruction)
+           print("error in instruction "+ instruction_string+" syntax")
+    elif instruction[0]=="xor":
+       if three_operand_check(instruction):
+           binary_instruction="010110"
+           reg1=to_binary(instruction[1][1],3)
+           reg2=to_binary(instruction[2][1],3)
+           reg3=to_binary(instruction[3][1],3)
+           binary_instruction=binary_instruction+reg1+reg2+reg3+"0"
+           binary_codes.append(binary_instruction)
+       else:
+           instruction_string = ' '.join(instruction)
+           print("error in instruction "+ instruction_string+" syntax")
+    elif instruction[0]=="cmp":
+       if two_operand_check(instruction):
+           binary_instruction="010111"
+           reg1=to_binary(instruction[1][1],3)
+           reg2=to_binary(instruction[2][1],3)
+           binary_instruction=binary_instruction+"000"+reg1+reg2+"0"
+           binary_codes.append(binary_instruction)
+       else:
+           instruction_string = ' '.join(instruction)
+           print("error in instruction "+ instruction_string+" syntax")
+    elif instruction[0]=="bitset":
+       if bitset_one_operand_check(instruction):
+           binary_instruction="011000"
+           reg1=to_binary(instruction[1][1],3)
+           imm=to_binary(instruction[2],16)
+           binary_instruction=binary_instruction+reg1+reg1+"0000"
+           binary_codes.append(binary_instruction)
+           binary_codes.append(imm)
+       else:
+           instruction_string = ' '.join(instruction)
+           print("error in instruction "+ instruction_string+" syntax")
+    elif instruction[0]=="rcl":
+       if imm_one_operand_check(instruction):
+           binary_instruction="011001"
+           reg1=to_binary(instruction[1][1],3)
+           imm=to_binary(instruction[2],16)
+           binary_instruction=binary_instruction+reg1+reg1+"0000"
+           binary_codes.append(binary_instruction)
+           binary_codes.append(imm)
+       else:
+           instruction_string = ' '.join(instruction)
+           print("error in instruction "+ instruction_string+" syntax")
+    elif instruction[0]=="rcr":
+       if imm_one_operand_check(instruction):
+           binary_instruction="011010"
+           reg1=to_binary(instruction[1][1],3)
+           imm=to_binary(instruction[2],16)
+           binary_instruction=binary_instruction+reg1+reg1+"0000"
+           binary_codes.append(binary_instruction)
+           binary_codes.append(imm)
+       else:
+           instruction_string = ' '.join(instruction)
+           print("error in instruction "+ instruction_string+" syntax")
 
 file_path = 'binary.txt'
 
