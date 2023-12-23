ea4fd74d94bd5da416ef70ceea6cf40cbc38253e
fatmaebrahim <fatmaebrahim3131@gmail.com>
2023-12-23 02:32:11 +0200

    initial forwarding unit

--- AluOperandsSel.vhd
+++ AluOperandsSel.vhd
@@ -3,11 +3,17 @@ USE ieee.std_logic_1164.ALL;
 USE IEEE.numeric_std.ALL;
 ENTITY AluOperandsSel IS
     PORT (
-        readData : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+        readData : IN STD_LOGIC_VECTOR (31 DOWNTO 0);--op2
+        readData1 : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
         immData : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
         opCode : IN STD_LOGIC_VECTOR (5 DOWNTO 0);
         lastOpCode : IN STD_LOGIC_VECTOR (5 DOWNTO 0);
+        AlutoAluop1 : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+        AlutoAluop2 : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
         Sel : IN STD_LOGIC;
+        forwardingsignalop1:in std_logic_vector(1 downto 0); --alu-mem
+        forwardingsignalop2:in std_logic_vector(1 downto 0); --alu-mem
+        op1 : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
         op2 : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
         opcodeAlu : OUT STD_LOGIC_VECTOR(5 DOWNTO 0)
     );
@@ -19,4 +25,7 @@ BEGIN
         (immData'RANGE => immData(15)) & immData;
     opcodeAlu <= opCode WHEN Sel = '0' ELSE
         lastOpCode;
+    op1 <=AlutoAluop1 when forwardingsignalop1(0) ='1' else
+        readData1;
+
 END archAluOperandsSel;
---
+++ ForwardingUnit.vhd
@@ -0,0 +1,27 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE IEEE.numeric_std.ALL;
+
+ENTITY ForwardingUnit IS
+    PORT (
+        Rsrc1 : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+        Rsrc2 : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+        RegExecuteMem : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+        RegMemWb : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+        forwardingsignalsop1 : Out STD_LOGIC_VECTOR(1 DOWNTO 0); --alu mem
+        forwardingsignalsop2 : Out STD_LOGIC_VECTOR(1 DOWNTO 0) --alu mem
+    );
+END ForwardingUnit;
+
+ARCHITECTURE archForwardingUnit OF ForwardingUnit IS
+BEGIN
+
+    forwardingsignalsop1(0) <= '1' WHEN Rsrc1 = RegExecuteMem ELSE
+    '0';
+    forwardingsignalsop1(1) <= '1' WHEN Rsrc1 = RegMemWb ELSE
+    '0';
+    forwardingsignalsop2(0) <= '1' WHEN Rsrc2 = RegExecuteMem ELSE
+    '0';
+    forwardingsignalsop2(1) <= '1' WHEN Rsrc2 = RegMemWb ELSE
+    '0';
+END archForwardingUnit;
