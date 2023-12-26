ed01e2d82d3f812d80cf5b26d4c59c1f3eaf74b9
fatmaebrahim <fatmaebrahim3131@gmail.com>
2023-12-26 19:08:36 +0200

    interrupt not tested

--- Fetch.vhd
+++ Fetch.vhd
@@ -6,15 +6,17 @@ USE work.my_pkg.ALL;
 -- get instruction from memory and update pc 
 ENTITY Fetch IS
     PORT (
-        clk                : IN STD_LOGIC;
+        clk : IN STD_LOGIC;
+        interrupt : IN STD_LOGIC;
+        memoryloc : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
         Instruction_Memory : IN memory_array(0 TO 4095)(15 DOWNTO 0);
-        reset              : IN STD_LOGIC;
-        JZ                 : IN STD_LOGIC;
-        JZ_PC              : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        Jump               : IN STD_LOGIC;
-        Jump_PC            : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        instruction        : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
-        PC                 : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
+        reset : IN STD_LOGIC;
+        JZ : IN STD_LOGIC;
+        JZ_PC : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        Jump : IN STD_LOGIC;
+        Jump_PC : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        instruction : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+        PC : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
     );
 END ENTITY Fetch;
 
@@ -23,34 +25,40 @@ ARCHITECTURE FetchArch OF Fetch IS
     -- signal Instruction_Memory : memory_array(0 TO 4095)(15 DOWNTO 0);
 
 BEGIN
-    PROCESS (reset, clk)
+    PROCESS (reset, clk, interrupt)
         VARIABLE jump_value : INTEGER;
-        VARIABLE jz_value   : INTEGER;
-        VARIABLE pc_value   : STD_LOGIC_VECTOR(31 DOWNTO 0);
+        VARIABLE jz_value : INTEGER;
+        VARIABLE pc_value : STD_LOGIC_VECTOR(31 DOWNTO 0);
     BEGIN
         PC <= pc_value;
         jump_value := to_integer(unsigned(Jump_PC));
-        jz_value   := to_integer(unsigned(JZ_PC));
-        IF reset = '1' AND clk = '1' THEN
-            -- PC <= (OTHERS       => '0');
-            pc_value := (OTHERS    => '0');
-            instruction <= (OTHERS => 'X');
+        jz_value := to_integer(unsigned(JZ_PC));
+        IF interrupt = '1' THEN
+            pc_value := memoryloc;
+            instruction <= Instruction_Memory(to_integer(unsigned(pc_value)));
+            pc_value := STD_LOGIC_VECTOR(unsigned(pc_value) + 1);
         ELSE
-            IF clk = '1' THEN
-                IF JZ = '1' AND jz_value >= 0 AND jz_value < 4096 THEN
-                    -- Check if index is within the valid range
-                    pc_value := (STD_LOGIC_VECTOR(to_unsigned(jz_value, 32)));
-                    instruction <= Instruction_Memory(jz_value);
-                    pc_value := STD_LOGIC_VECTOR(unsigned(pc_value) + 1);
-                ELSE
-                    IF Jump = '1' AND jump_value >= 0 AND jump_value < 4096 THEN
-                        pc_value := (STD_LOGIC_VECTOR(to_unsigned(jump_value, 32)));
-                        instruction <= Instruction_Memory(jump_value);
+            IF reset = '1' AND clk = '1' THEN
+                -- PC <= (OTHERS       => '0');
+                pc_value := (OTHERS => '0');
+                instruction <= (OTHERS => 'X');
+            ELSE
+                IF clk = '1' THEN
+                    IF JZ = '1' AND jz_value >= 0 AND jz_value < 4096 THEN
+                        -- Check if index is within the valid range
+                        pc_value := (STD_LOGIC_VECTOR(to_unsigned(jz_value, 32)));
+                        instruction <= Instruction_Memory(jz_value);
                         pc_value := STD_LOGIC_VECTOR(unsigned(pc_value) + 1);
                     ELSE
-                        -- PC          <= STD_LOGIC_VECTOR(unsigned(PC) + 1);
-                        instruction <= Instruction_Memory(to_integer(unsigned(pc_value)));
-                        pc_value := STD_LOGIC_VECTOR(unsigned(pc_value) + 1);
+                        IF Jump = '1' AND jump_value >= 0 AND jump_value < 4096 THEN
+                            pc_value := (STD_LOGIC_VECTOR(to_unsigned(jump_value, 32)));
+                            instruction <= Instruction_Memory(jump_value);
+                            pc_value := STD_LOGIC_VECTOR(unsigned(pc_value) + 1);
+                        ELSE
+                            -- PC          <= STD_LOGIC_VECTOR(unsigned(PC) + 1);
+                            instruction <= Instruction_Memory(to_integer(unsigned(pc_value)));
+                            pc_value := STD_LOGIC_VECTOR(unsigned(pc_value) + 1);
+                        END IF;
                     END IF;
                 END IF;
             END IF;
--- IF_ID_Reg.vhd
+++ IF_ID_Reg.vhd
@@ -4,62 +4,70 @@ USE IEEE.std_logic_1164.ALL;
 USE IEEE.numeric_std.ALL;
 ENTITY IF_ID_Reg IS
     PORT (
-        clk         : IN STD_LOGIC;
-        reset       : IN STD_LOGIC;
-        PC_IN       : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+        clk : IN STD_LOGIC;
+        reset : IN STD_LOGIC;
+        interrupt : IN STD_LOGIC;
+        PC_IN : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
         Instruction : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-        readReg0    : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
-        readReg1    : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
-        writeReg0   : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
-        writeReg1   : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
-        opCode      : OUT STD_LOGIC_VECTOR (5 DOWNTO 0);
-        writeData0  : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
-        writeData1  : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
-        ImmEaValue  : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
-        PC_OUT      : OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
+        readReg0 : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
+        readReg1 : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
+        writeReg0 : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
+        writeReg1 : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
+        opCode : OUT STD_LOGIC_VECTOR (5 DOWNTO 0);
+        writeData0 : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
+        writeData1 : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
+        ImmEaValue : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
+        PC_OUT : OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
 
     );
 END ENTITY IF_ID_Reg;
 ARCHITECTURE IF_ID_RegArch OF IF_ID_Reg IS
 
 BEGIN
-    PROCESS (clk, reset)
+    PROCESS (clk, interrupt,reset)
     BEGIN
         IF reset = '1' THEN
-            opcode    <= "111111";
+            opcode <= "111111";
             writeReg0 <= (OTHERS => 'X');
-            readReg0  <= (OTHERS => 'X');
-            readReg1  <= (OTHERS => 'X');
+            readReg0 <= (OTHERS => 'X');
+            readReg1 <= (OTHERS => 'X');
         ELSE
-            IF clk = '1' THEN
-                ImmEaValue <= Instruction;
-                CASE Instruction(15 DOWNTO 14) IS
-                    WHEN "00" => --one operand
-                        opCode    <= Instruction(15 DOWNTO 10);
-                        writeReg0 <= Instruction(9 DOWNTO 7);
-                        readReg0  <= Instruction(6 DOWNTO 4);
-                    WHEN "01" => --two operands
-                        opCode    <= Instruction(15 DOWNTO 10);
-                        writeReg0 <= Instruction(9 DOWNTO 7);
-                        readReg0  <= Instruction(6 DOWNTO 4);
-                        readReg1  <= Instruction(3 DOWNTO 1);
-                        writeReg1 <= Instruction(6 DOWNTO 4);
-                    WHEN "10" => --memory
-                        opCode    <= Instruction(15 DOWNTO 10);
-                        writeReg0 <= Instruction(9 DOWNTO 7);
-                        readReg0  <= Instruction(6 DOWNTO 4);
-                        readReg1  <= Instruction(3 DOWNTO 1);
-                    WHEN "11" => --branch
-                        opCode    <= Instruction(15 DOWNTO 10);
-                        writeReg0 <= Instruction(9 DOWNTO 7);
-                        readReg0  <= Instruction(6 DOWNTO 4);
-                        readReg1  <= Instruction(3 DOWNTO 1);
-                    WHEN OTHERS          =>
-                        opCode    <= (OTHERS => 'X');
-                        writeReg0 <= (OTHERS => 'X');
-                        readReg0  <= (OTHERS => 'X');
-                        readReg1  <= (OTHERS => 'X');
-                END CASE;
+            IF interrupt = '1' THEN
+                opcode <= "111110";
+                writeReg0 <= (OTHERS => 'X');
+                readReg0 <= (OTHERS => 'X');
+                readReg1 <= (OTHERS => 'X');
+            ELSE
+                IF clk = '1' THEN
+                    ImmEaValue <= Instruction;
+                    CASE Instruction(15 DOWNTO 14) IS
+                        WHEN "00" => --one operand
+                            opCode <= Instruction(15 DOWNTO 10);
+                            writeReg0 <= Instruction(9 DOWNTO 7);
+                            readReg0 <= Instruction(6 DOWNTO 4);
+                        WHEN "01" => --two operands
+                            opCode <= Instruction(15 DOWNTO 10);
+                            writeReg0 <= Instruction(9 DOWNTO 7);
+                            readReg0 <= Instruction(6 DOWNTO 4);
+                            readReg1 <= Instruction(3 DOWNTO 1);
+                            writeReg1 <= Instruction(6 DOWNTO 4);
+                        WHEN "10" => --memory
+                            opCode <= Instruction(15 DOWNTO 10);
+                            writeReg0 <= Instruction(9 DOWNTO 7);
+                            readReg0 <= Instruction(6 DOWNTO 4);
+                            readReg1 <= Instruction(3 DOWNTO 1);
+                        WHEN "11" => --branch
+                            opCode <= Instruction(15 DOWNTO 10);
+                            writeReg0 <= Instruction(9 DOWNTO 7);
+                            readReg0 <= Instruction(6 DOWNTO 4);
+                            readReg1 <= Instruction(3 DOWNTO 1);
+                        WHEN OTHERS =>
+                            opCode <= (OTHERS => 'X');
+                            writeReg0 <= (OTHERS => 'X');
+                            readReg0 <= (OTHERS => 'X');
+                            readReg1 <= (OTHERS => 'X');
+                    END CASE;
+                END IF;
             END IF;
         END IF;
     END PROCESS;
